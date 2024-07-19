614f9ba5f899ce0b4e555d64e6f3bb67d0666a0e
anagha012004 <anaghabadhe3@gmail.com>
2024-07-19 17:36:34 +0530

    -5

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,19 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const connectDB =async () => {
-    try {
-        const conn= await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.log(`Error: ${error.message}`);
-        process.exit(1);
-    }
- };
+const connectDB = async () => {
+  try {
+    console.log('MONGO_URI:', process.env.MONGO_URI); // Debugging line
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
 export default connectDB;
