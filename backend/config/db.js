495e94b2094fa07d87dbb3a1ec851e9c02cfcd9f
anagha012004 <anaghabadhe3@gmail.com>
2025-12-30 09:41:13 +0530

    Add production seeder script

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,11 +4,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    console.log('MONGO_URI:', process.env.MONGO_URI); // Debugging line
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
